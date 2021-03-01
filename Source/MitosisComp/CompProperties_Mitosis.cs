967785e18c8c1d60cc25d6f0e39ce6d78f567f3c
emipa606 <emil.offermo@litet.net>
2021-03-01 10:30:50 +0100

    First publish

---
+++ Source/MitosisComp/CompProperties_Mitosis.cs
@@ -0,0 +1,32 @@
+﻿using Verse;
+
+namespace MitosisComp
+{
+    // Token: 0x02000002 RID: 2
+    public class CompProperties_Mitosis : CompProperties
+    {
+        // Token: 0x04000005 RID: 5
+        public readonly float coldMultiplier = 0f;
+
+        // Token: 0x04000006 RID: 6
+        public readonly int countLimit = 40;
+
+        // Token: 0x04000004 RID: 4
+        public readonly float heatMultiplier = 0f;
+
+        // Token: 0x04000001 RID: 1
+        public readonly PawnKindDef pawnKind = null;
+
+        // Token: 0x04000002 RID: 2
+        public readonly int splittingTimer = 60000;
+
+        // Token: 0x04000003 RID: 3
+        public readonly float temperatureOffset = 0f;
+
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        public CompProperties_Mitosis()
+        {
+            compClass = typeof(Comp_Mitosis);
+        }
+    }
+}
---
+++ Source/MitosisComp/Comp_Mitosis.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace MitosisComp
+{
+    public class Comp_Mitosis : ThingComp
+    {
+        private int ticksSinceLastSplit;
+
+        private CompProperties_Mitosis Props => (CompProperties_Mitosis) props;
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref ticksSinceLastSplit, "ticksSinceLastSplit");
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            if (!Prefs.DevMode)
+            {
+                return string.Empty;
+            }
+
+            var text = "Last Mitosis Split: " + ticksSinceLastSplit + " ticks";
+            text = text + "\nWill split at: " + (Props.splittingTimer * GetTemperatureMultiplier()) + " ticks";
+            text = text + "\nCan Split:" + CanSplit();
+
+            return text;
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            var flag = parent.Map != null;
+            if (!flag)
+            {
+                return;
+            }
+
+            var flag2 = ticksSinceLastSplit >= Props.splittingTimer * GetTemperatureMultiplier() && CanSplit();
+            if (flag2)
+            {
+                SpawnPawn();
+                ticksSinceLastSplit = 0;
+            }
+            else
+            {
+                ticksSinceLastSplit++;
+            }
+        }
+
+        private bool CanSplit()
+        {
+            var pawns = from p in parent.Map.mapPawns?.AllPawns
+                where p != null && p.def == Props.pawnKind.race
+                select p;
+            var num = pawns.Count();
+            return Props.countLimit > num;
+        }
+
+        private float GetTemperatureMultiplier()
+        {
+            var flag = parent.AmbientTemperature >= 0f;
+            float result;
+            if (flag)
+            {
+                result = 1f - (Props.heatMultiplier * (parent.AmbientTemperature - Props.temperatureOffset));
+            }
+            else
+            {
+                result = 1f - (Props.coldMultiplier * (parent.AmbientTemperature - Props.temperatureOffset));
+            }
+
+            return result;
+        }
+
+        private void SpawnPawn()
+        {
+            var request = new PawnGenerationRequest(Props.pawnKind, parent.Faction, PawnGenerationContext.NonPlayer, -1,
+                true, true, false, false, true, false, 1f, false, true, true, false);
+            var newThing = PawnGenerator.GeneratePawn(request);
+            GenSpawn.Spawn(newThing, parent.Position, parent.Map);
+        }
+    }
+}
